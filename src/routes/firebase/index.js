28eedf9fb1c33b8eb0a1277fbe905fd2793f1199
Domonkos Papp <pappdomonkos96@gmail.com>
2021-12-07 18:38:47 +0100

    Rename

--- src/routes/firebase/index.js
+++ src/routes/firebase/index.js
@@ -4,7 +4,7 @@ import BookCreate from "../../components/BookCreate";
 const Firebase = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Books</h1>
       <h2>List of books</h2>
       <BookList />
       <h2>Create a new book</h2>
