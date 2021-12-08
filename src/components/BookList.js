0de3c667f9a27b5853ccf1bb4955bd3c24b0da9c
Domonkos Papp <pappdomonkos96@gmail.com>
2021-12-08 10:56:13 +0100

    Fix infinity loop and add open button

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,27 @@
 import { collection, onSnapshot, query, where } from "firebase/firestore"
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import db from "../utils/firebase";
+import BookOpenButton from "./BookOpenButton";
 import BookRemoveButton from "./BookRemoveButton";
 
 const BookList = ({authorFilter}) => {
     const[books, setBooks] = useState([])
     const collectionRef = collection(db, "books")
-    const q = authorFilter ? query(collectionRef, authorFilter && where("author", "==", authorFilter)) : collectionRef;
+    const q = authorFilter ? query(collectionRef, authorFilter && where("author", "==", authorFilter)) : collectionRef;    
 
     const updateBooks = (snapshot) => {
         setBooks(snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})))
     }
-
-    onSnapshot(q, updateBooks)
+    
+    useEffect(() => {
+        onSnapshot(q, updateBooks)
+    },[])
 
     const renderBook = (book, index) => (
         <p key={index}>
             {`${book.title} ${book.author} `}
+            <button onClick={open}>open</button>
+            <BookOpenButton bookId={book.id} />
             <BookRemoveButton bookId={book.id} />
         </p>
     )
